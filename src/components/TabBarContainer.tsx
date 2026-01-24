2010a62caab20751c9ed517e7f2b3c1c45544e7b
Andree Hultgren <andree.hultgren@arkion.co>
2026-01-24 20:43:48 +0100

    fix: Tab container sizing

--- src/components/TabBarContainer.tsx
+++ src/components/TabBarContainer.tsx
@@ -1,14 +1,15 @@
 import styled from "styled-components";
 
 export default styled.div`
-
 	display: flex;
-	flex: 1;
+	background: #252525;
+	border-bottom: 1px solid #3d3d3d;
+	min-height: 36px;
+	align-items: stretch;
 	overflow-x: auto;
 	overflow-y: hidden;
 
-
-  &::-webkit-scrollbar {
-    height: 4px;
-  }
+	&::-webkit-scrollbar {
+		height: 4px;
+	}
 `;
